a6580eb347627bc938c400e40f402094f20c1629
Eason0210 <aqua0210@163.com>
2019-06-01 11:02:48 +0800

    PhoneNumer

---
+++ PhoneNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace PhoneNumber{
+    class Program{
+        static void Main(string[] args){
+            var phoneNumber = "45683926";
+            var solution = new Solution();
+            var result = solution.Combinations(phoneNumber);
+            File.WriteAllLines(@"C:\Temp\output.txt", result);
+        }
+    }
+}
